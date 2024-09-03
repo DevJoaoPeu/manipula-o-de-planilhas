0ffc35d52a57085c7da7c69714c3b11bd60f582c
DevJoaoPeu <joaopedroprog.contato@gmail.com>
2024-09-03 20:39:41 -0300

    refactor: refatorando codigo benef gndi

--- src/GNDI/benef.js
+++ src/GNDI/benef.js
@@ -3,115 +3,114 @@ import path from "path";
 import fs from "fs";
 import { convertNumberFormat, formatDate } from "./utils/util.js";
 
-// Função para ler um arquivo .mdb e salvar os dados em um arquivo .txt formatado por tabulação
-async function readMdbFile(filePath) {
-  try {
-    const connection = ADODB.open(
-      `Provider=Microsoft.Jet.OLEDB.4.0;Data Source=${filePath};`
-    );
-
-    const tables = await connection.schema(20); // 20: adSchemaTables
-    if (tables.length > 0) {
-      const tabelaNome = "Cadastro De Associados";
-      const query = `SELECT * FROM [${tabelaNome}]`;
-
-      try {
-        const rows = await connection.query(query);
-
-        if (rows.length > 0) {
-          // Pega os cabeçalhos das colunas e corrige a codificação
-          const headers = Object.keys(rows[0]);
-
-          // Adiciona o cabeçalho como primeira linha
-          let data = headers.join("\t") + "\n";
-
-          // Adiciona cada linha de dados
-          rows.forEach((row) => {
-            const rowData = headers.map((header) => {
-              let value = row[header];
-
-              // Verifica e formata as colunas específicas de data
-              if (
-                [
-                  "Data de Nascimento",
-                  "Data do Atendimento",
-                  "Competência",
-                  "Data de Adesão ao Plano",
-                  "Data de Admissão do Empregado",
-                  "Data de Cancelamento",
-                ].includes(header)
-              ) {
-                value = formatDate(value);
-              }
-
-              // Converte ponto para vírgula em coluna de valor
-              if (header.toLowerCase() === "valor") {
-                value = convertNumberFormat(value);
-              }
-
-              return value;
-            });
-            data += rowData.join("\t") + "\n";
-          });
-
-          // Caminho para o arquivo .txt
-          const baseName = path.basename(filePath, ".mdb");
-          const txtFilePath = path.join(
-            path.dirname(filePath),
-            `${datePath}_Cadastro De Associados ${baseName}.txt`
-          );
-
-          // Escrevendo os dados no arquivo .txt
-          fs.writeFileSync(txtFilePath, data, "latin1");
-
-          console.log(
-            `Dados da tabela ${tabelaNome} salvos em: ${txtFilePath}`
-          );
-        } else {
-          console.log(
-            `Nenhuma linha de dados encontrada na tabela ${tabelaNome} do arquivo ${filePath}`
-          );
-        }
-      } catch (queryError) {
-        console.error(
-          `Erro ao executar a consulta para a tabela ${tabelaNome}:`,
-          queryError
-        );
-      }
-    } else {
-      console.log(`Nenhuma tabela encontrada no arquivo ${filePath}`);
-    }
-  } catch (error) {
-    console.error(`Erro ao ler o arquivo ${filePath}:`, error);
-  }
-}
-
-// Função para ler todos os arquivos .mdb em um diretório
-async function readAllMdbFiles(directoryPath) {
-  try {
-    // Lendo todos os arquivos no diretório
-    const files = fs.readdirSync(directoryPath);
-
-    // Filtrando apenas arquivos com a extensão .mdb
-    const mdbFiles = files.filter((file) => path.extname(file) === ".mdb");
-
-    // Lendo cada arquivo .mdb
-    for (const file of mdbFiles) {
-      const filePath = path.join(directoryPath, file);
-      await readMdbFile(filePath);
-    }
-  } catch (error) {
-    console.error("Erro ao ler os arquivos do diretório:", error);
-  }
-}
-
-// Obtendo o caminho do diretório do terminal
 const directoryPath = process.argv[3];
 const datePath = process.argv[2];
-if (!directoryPath) {
-  console.error("Por favor, forneça o caminho do diretório como argumento.");
-  process.exit(1);
+class BenefGndi {
+  async verifyPath(directoryPath) {
+    if (!directoryPath) {
+      console.error(
+        "Por favor, forneça o caminho do diretório como argumento."
+      );
+      process.exit(1);
+    }
+
+    await this.readAllMdbFiles(path.resolve(directoryPath));
+  }
+
+  async readAllMdbFiles(directoryPath) {
+    try {
+      const files = fs.readdirSync(directoryPath);
+
+      const mdbFiles = files.filter((file) => path.extname(file) === ".mdb");
+
+      for (const file of mdbFiles) {
+        const filePath = path.join(directoryPath, file);
+        await this.readMdbFile(filePath);
+      }
+    } catch (error) {
+      console.error("Erro ao ler os arquivos do diretório:", error);
+    }
+  }
+
+  async readMdbFile(filePath) {
+    try {
+      const connection = ADODB.open(
+        `Provider=Microsoft.Jet.OLEDB.4.0;Data Source=${filePath};`
+      );
+
+      const tables = await connection.schema(20); // 20: adSchemaTables
+      if (tables.length > 0) {
+        const tabelaNome = "Cadastro De Associados";
+        const query = `SELECT * FROM [${tabelaNome}]`;
+
+        try {
+          const rows = await connection.query(query);
+
+          if (rows.length > 0) {
+            // Pega os cabeçalhos das colunas e corrige a codificação
+            const headers = Object.keys(rows[0]);
+
+            // Adiciona o cabeçalho como primeira linha
+            let data = headers.join("\t") + "\n";
+
+            rows.forEach((row) => {
+              const rowData = headers.map((header) => {
+                let value = row[header];
+
+                // Verifica e formata as colunas específicas de data
+                if (
+                  [
+                    "Data de Nascimento",
+                    "Data do Atendimento",
+                    "Competência",
+                    "Data de Adesão ao Plano",
+                    "Data de Admissão do Empregado",
+                    "Data de Cancelamento",
+                  ].includes(header)
+                ) {
+                  value = formatDate(value);
+                }
+
+                // Converte ponto para vírgula em coluna de valor
+                if (header.toLowerCase() === "valor") {
+                  value = convertNumberFormat(value);
+                }
+
+                return value;
+              });
+              data += rowData.join("\t") + "\n";
+            });
+
+            const baseName = path.basename(filePath, ".mdb");
+            const txtFilePath = path.join(
+              path.dirname(filePath),
+              `${datePath}_Cadastro De Associados ${baseName}.txt`
+            );
+
+            // Escrevendo os dados no arquivo .txt
+            fs.writeFileSync(txtFilePath, data, "latin1");
+
+            console.log(
+              `Dados da tabela ${tabelaNome} salvos em: ${txtFilePath}`
+            );
+          } else {
+            console.log(
+              `Nenhuma linha de dados encontrada na tabela ${tabelaNome} do arquivo ${filePath}`
+            );
+          }
+        } catch (queryError) {
+          console.error(
+            `Erro ao executar a consulta para a tabela ${tabelaNome}:`,
+            queryError
+          );
+        }
+      } else {
+        console.log(`Nenhuma tabela encontrada no arquivo ${filePath}`);
+      }
+    } catch (error) {
+      console.error(`Erro ao ler o arquivo ${filePath}:`, error);
+    }
+  }
 }
 
-// Chamando a função para ler todos os arquivos .mdb no diretório
-readAllMdbFiles(path.resolve(directoryPath));
+new BenefGndi().verifyPath(directoryPath);
